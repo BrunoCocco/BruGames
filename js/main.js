227216995982e2e0fe15a0205779f7367f02f8bc
Bruno Cocco <Brunoe.cocco@gmail.com>
2022-03-22 01:17:56 -0300

    creando piedra papel y tijera beta no optimo

---
+++ js/main.js
@@ -0,0 +1,59 @@
+const opciones= ["piedra","papel","tijera"]
+
+//TODO: encapsular
+const piedra = document.getElementById("piedra");
+const papel = document.getElementById("papel");
+const tijera = document.getElementById("tijera");
+
+
+
+piedra.addEventListener("click", ()=>{
+    const user = "piedra"
+    const random = Math.floor(Math.random()*opciones.length);
+    let elecionPc = opciones[random]
+    console.log(elecionPc);
+
+    if (elecionPc === user) {
+        console.log("empate")
+    }
+    else if (elecionPc == "piedra" && user == "papel", elecionPc == "papel" && user == "tijera", elecionPc == "tijera" && user == "piedra"){
+        console.log("usuario Gana")
+    } else {
+        console.log("usuario pierde")
+    }
+})
+
+
+
+papel.addEventListener("click", ()=>{
+    const user = "piedra"
+    const random = Math.floor(Math.random()*opciones.length);
+    let elecionPc = opciones[random]
+    console.log(elecionPc);
+
+    if (elecionPc === user) {
+        console.log("empate")
+    }
+    else if (elecionPc == "piedra" && user == "papel", elecionPc == "papel" && user == "tijera", elecionPc == "tijera" && user == "piedra"){
+        console.log("usuario Gana")
+    } else {
+        console.log("usuario pierde")
+    }
+})
+
+
+tijera.addEventListener("click", ()=>{
+    const user = "piedra"
+    const random = Math.floor(Math.random()*opciones.length);
+    let elecionPc = opciones[random]
+    console.log(elecionPc);
+
+    if (elecionPc === user) {
+        console.log("empate")
+    }
+    else if (elecionPc == "piedra" && user == "papel", elecionPc == "papel" && user == "tijera", elecionPc == "tijera" && user == "piedra"){
+        console.log("usuario Gana")
+    } else {
+        console.log("usuario pierde")
+    }
+})
