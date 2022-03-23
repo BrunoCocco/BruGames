716d42db10991cd89a2c829bc3d39dd156b691d3
Bruno Cocco <Brunoe.cocco@gmail.com>
2022-03-23 03:20:43 -0300

    mejorando el codigo

--- generala/main.js
+++ generala/main.js
@@ -1,5 +1,5 @@
 const opciones = [1, 2, 3, 4, 5, 6];
-const jugada = [];
+const tiradaDeDado = [];
 
 class newJugada {
     constructor(dado) {
@@ -11,9 +11,17 @@ const tirarDadoRamdom = document.getElementById("tirarDado");
 console.log(tirarDadoRamdom);
 
 tirarDadoRamdom.addEventListener("click", () => {
-    let jugada = new newJugada();
-    jugada.dado =Math.floor( Math.random(1,6) * opciones.length + 1);
+    for(i=0; i<6; i++){
+    let jugada = new newJugada();  
+    jugada.dado = Math.floor( Math.random(1,6) * opciones.length + 1);
 
+    tiradaDeDado.push(jugada)
+}
+console.log(tiradaDeDado);
 
-    console.log(jugada);
+// if(tiradaDeDado.length > 6){
+
+//     console.log("no puede trar de nuevo")
+
+// }
 });
